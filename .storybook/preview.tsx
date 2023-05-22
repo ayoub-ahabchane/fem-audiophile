1dc643882cb5ef31c9742d6e5940e6b967542848
ayoub_ahabchane <mr.ayoub.ahabchane@gmail.com>
2023-05-22 18:23:58 +0100

    build: initial setup

---
+++ .storybook/preview.tsx
@@ -0,0 +1,46 @@
+import { withThemeByDataAttribute } from "@storybook/addon-styling";
+import type { Preview } from "@storybook/react";
+import { Manrope } from "next/font/google";
+import React from "react";
+import "../src/app/globals.css";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-manrope",
+});
+
+const preview: Preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      attributeName: "data-mode",
+    }),
+    (Story) => (
+      <main
+        className={`${manrope.variable}`}
+        style={{ fontFamily: "var(--font-manrope)" }}
+      >
+        <Story />
+      </main>
+    ),
+  ],
+};
+
+export default preview;
